019707c54833d78f4b9f87cc03f71817aef5abe8
Daniel <ohmyzazu@gmail.com>
2020-06-01 16:19:55 -0400

    second choice

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const speak = chalk.yellowBright;
 const choice = chalk.cyanBright;
 const action = chalk.gray;
 var name = ''
-cfonts.say('Untitled RPG', {
+//functions prints the game title using a cfonts template (node package).
+function printTitle(){
+cfonts.say('Untitled RPG|Demo', {
     font: 'simple3d',              // define the font face
     align: 'left',              // define text alignment
     colors: ['black','white'],         // define all colors
@@ -22,13 +24,14 @@ cfonts.say('Untitled RPG', {
     independentGradient: false, // define if you want to recalculate the gradient for each new line
     transitionGradient: false,  // define if this is a transition between colors directly
     env: 'node'                 // define the environment CFonts is being executed in
-});
-//Intro
-cfonts.render("Untitled RPG")
+});};
+//Title Screen
+printTitle()
 log("You wake up in a soft bed. Your surroundings appear to be that of a small house, potentially a cottage. Outside, the sky is almost dark, and the sun is setting. Hovering right above you is- \nA Fairy?")
 log(speak("Hello there! My name’s Jasmine! What’s yours?"));
 name = prompt("Your name: ");
 console.clear()
+//Intro
 log(speak(`Welcome to Alivia, ${name}! Do you know how you got here ?
     Oh, you don’t know ? Well, okay then! 
 You don’t know what Alivia is either ?
@@ -40,17 +43,27 @@ Together, we form the Cardinal Union.
 Anyways, because Alivia is a border city, we have a lot of people going on expeditions to mine and to loot.That’s probably your best start, ${name}!
 I’ll leave you alone for now!`
 ));
+//choice 1
 log("As you sit up, you notice your muscles are aching. You probably won’t be moving too much for today. It’s almost completely dark anyways.");
 log(`Looking around the room, you notice a desk with an ornate set of stationery beside the bed. On the opposite side, a closet. The floor was made of wood, probably oak}. The walls were a light beige, with wooden beams running up the sides.`);
 log(choice("Where do you want to go?\n\n Downstairs\n Outside"));
-firstChoice = prompt("You decide to go: ");
+let firstChoice = prompt("You decide to go: ");
 if (firstChoice.toLowerCase() === "downstairs"){
-    log(action("You head off "))
+    log(action("You creep out of your room and walk downstairs."))
 } else if (firstChoice.toLowerCase() === "outside"){
     log(action("You get off the bed and head for the door. You look down and realize that you don't have any shoes. You decide to go downstairs to see if anyone has some."))
 } else{
-    animate.glitch("You must have hit your head pretty hard... you don't know where that is!");
-    setTimeout(()=>{log(action("You go downstairs."))},2000);
+    log(action("You must have hit your head pretty hard... you don't know where that is!"));
 }
-
-log("As you go downstairs, you hear the cries and cheers of people. You wonder what they're celebrating.\n\nAs the picture settles into your view, ")
+//choice 2
+log("As you go downstairs, you hear the cries and cheers of people. You wonder what they're celebrating.\n\nAs your eyes adjust to the lighting, you see around 20 people around a series of tables, eating and drinking.\nThey appear to be a group of warriors.");
+log(choice("What do you want to do?\n\n Find someone and ask for help\n Wait in the hallway for someone to help you."));
+let secondChoice = prompt("What do you do? (Ask or Wait): ");
+if (secondChoice.toLowerCase() === "ask"){
+    log(action("You go around the tables and ask some of the warriors if they know who rescued you."));
+} else if (secondChoice.toLowerCase() === "wait"){
+    log(action("You wait around for an hour or two..."));
+} else{
+    log(action("There's no other course of action here!"));
+    log(action("You decide to wait."));
+}
